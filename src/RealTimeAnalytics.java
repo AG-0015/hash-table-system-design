53e520fc246d5d902515dfde8ab90fbea6d4cd9e
AG-0015 <agajalakshmi07@gmail.com>
2026-03-14 12:54:32 +0530

    Problem5: Real time analytics counter

--- src/RealTimeAnalytics.java
+++ src/RealTimeAnalytics.java
@@ -1,2 +1,25 @@
-public class RealTimeAnalytics {
+import java.util.*;
+
+class AnalyticsDashboard {
+
+    HashMap<String, Integer> pageViews = new HashMap<>();
+    HashMap<String, Set<String>> uniqueVisitors = new HashMap<>();
+    HashMap<String, Integer> trafficSource = new HashMap<>();
+
+    public void processEvent(String url, String userId, String source) {
+
+        pageViews.put(url, pageViews.getOrDefault(url, 0) + 1);
+
+        uniqueVisitors.putIfAbsent(url, new HashSet<>());
+        uniqueVisitors.get(url).add(userId);
+
+        trafficSource.put(source,
+                trafficSource.getOrDefault(source, 0) + 1);
+    }
+
+    public void displayDashboard() {
+
+        System.out.println("Page Views: " + pageViews);
+        System.out.println("Traffic Sources: " + trafficSource);
+    }
 }
