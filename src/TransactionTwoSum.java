dfebea073114658bb680b5b73b80ac193ea25a69
AG-0015 <agajalakshmi07@gmail.com>
2026-03-14 13:10:59 +0530

    Problem9: Transaction two sum detection

--- src/TransactionTwoSum.java
+++ src/TransactionTwoSum.java
@@ -1,2 +1,33 @@
-public class TransactionTwoSum {
+import java.util.*;
+
+class Transaction {
+
+    int id;
+    int amount;
+
+    Transaction(int id, int amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+}
+
+class TwoSumDetector {
+
+    public void findTwoSum(List<Transaction> list, int target) {
+
+        HashMap<Integer, Transaction> map = new HashMap<>();
+
+        for (Transaction t : list) {
+
+            int complement = target - t.amount;
+
+            if (map.containsKey(complement)) {
+
+                System.out.println(
+                        map.get(complement).id + " + " + t.id);
+            }
+
+            map.put(t.amount, t);
+        }
+    }
 }
