f57733c7910f2247a17051ee939da0c71a0d241e
AG-0015 <agajalakshmi07@gmail.com>
2026-03-14 13:13:02 +0530

    Problem10: Multi level cache system

--- src/MultiLevelCache.java
+++ src/MultiLevelCache.java
@@ -1,2 +1,32 @@
-public class MultiLevelCache {
+import java.util.*;
+
+class MultiLevelCache {
+
+    LinkedHashMap<String, String> L1 =
+            new LinkedHashMap<>(10000, 0.75f, true);
+
+    HashMap<String, String> L2 = new HashMap<>();
+
+    HashMap<String, Integer> accessCount = new HashMap<>();
+
+    public String getVideo(String id) {
+
+        if (L1.containsKey(id)) {
+            return "L1 HIT";
+        }
+
+        if (L2.containsKey(id)) {
+
+            accessCount.put(id,
+                    accessCount.getOrDefault(id, 0) + 1);
+
+            if (accessCount.get(id) > 5) {
+                L1.put(id, L2.get(id));
+            }
+
+            return "L2 HIT";
+        }
+
+        return "L3 DATABASE HIT";
+    }
 }
