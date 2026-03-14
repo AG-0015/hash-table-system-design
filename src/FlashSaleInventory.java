28b84005971a957b6a40e445590985b860787d55
AG-0015 <agajalakshmi07@gmail.com>
2026-03-14 12:40:05 +0530

    Problem2: Flash sale inventory manager using HashMap

--- src/FlashSaleInventory.java
+++ src/FlashSaleInventory.java
@@ -1,2 +1,41 @@
-public class FlashSaleInventory {
+import java.util.*;
+
+class FlashSaleInventory {
+
+    private HashMap<String, Integer> stock = new HashMap<>();
+    private HashMap<String, Queue<Integer>> waitingList = new HashMap<>();
+
+    public void addProduct(String productId, int quantity) {
+        stock.put(productId, quantity);
+        waitingList.put(productId, new LinkedList<>());
+    }
+
+    public synchronized String purchaseItem(String productId, int userId) {
+
+        int currentStock = stock.getOrDefault(productId, 0);
+
+        if (currentStock > 0) {
+            stock.put(productId, currentStock - 1);
+            return "Success, remaining: " + (currentStock - 1);
+        }
+
+        waitingList.get(productId).add(userId);
+        return "Added to waiting list. Position: "
+                + waitingList.get(productId).size();
+    }
+
+    public int checkStock(String productId) {
+        return stock.getOrDefault(productId, 0);
+    }
+
+    public static void main(String[] args) {
+
+        FlashSaleInventory sale = new FlashSaleInventory();
+
+        sale.addProduct("IPHONE15", 2);
+
+        System.out.println(sale.purchaseItem("IPHONE15", 1));
+        System.out.println(sale.purchaseItem("IPHONE15", 2));
+        System.out.println(sale.purchaseItem("IPHONE15", 3));
+    }
 }
