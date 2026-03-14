a585f513348e7db0a736235b7095d3d9ee95bd3a
AG-0015 <agajalakshmi07@gmail.com>
2026-03-14 12:30:44 +0530

    Problem1: Username availability checker using HashMap

--- src/UsernameChecker.java
+++ src/UsernameChecker.java
@@ -1,2 +1,65 @@
-public class UsernameChecker {
+import java.util.*;
+
+class UsernameChecker {
+
+    private HashMap<String, Integer> usernameMap = new HashMap<>();
+    private HashMap<String, Integer> attemptFrequency = new HashMap<>();
+
+    public boolean checkAvailability(String username) {
+
+        attemptFrequency.put(username,
+                attemptFrequency.getOrDefault(username, 0) + 1);
+
+        return !usernameMap.containsKey(username);
+    }
+
+    public void registerUser(String username, int userId) {
+        usernameMap.put(username, userId);
+    }
+
+    public List<String> suggestAlternatives(String username) {
+
+        List<String> suggestions = new ArrayList<>();
+
+        for (int i = 1; i <= 3; i++) {
+            String suggestion = username + i;
+
+            if (!usernameMap.containsKey(suggestion))
+                suggestions.add(suggestion);
+        }
+
+        suggestions.add(username.replace("_", "."));
+
+        return suggestions;
+    }
+
+    public String getMostAttempted() {
+
+        String maxUser = "";
+        int max = 0;
+
+        for (String u : attemptFrequency.keySet()) {
+
+            if (attemptFrequency.get(u) > max) {
+                max = attemptFrequency.get(u);
+                maxUser = u;
+            }
+        }
+
+        return maxUser + " (" + max + " attempts)";
+    }
+
+    public static void main(String[] args) {
+
+        UsernameChecker system = new UsernameChecker();
+
+        system.registerUser("john_doe", 1);
+
+        System.out.println(system.checkAvailability("john_doe"));
+        System.out.println(system.checkAvailability("jane_smith"));
+
+        System.out.println(system.suggestAlternatives("john_doe"));
+
+        System.out.println(system.getMostAttempted());
+    }
 }
