f7a8870f32d9f7069d4154d594a4524628e9926a
AG-0015 <agajalakshmi07@gmail.com>
2026-03-14 13:08:39 +0530

    Problem7: Autocomplete system

--- src/AutocompleteSystem.java
+++ src/AutocompleteSystem.java
@@ -1,2 +1,32 @@
-public class AutocompleteSystem {
+import java.util.*;
+
+class AutocompleteSystem {
+
+    HashMap<String, Integer> queryFreq = new HashMap<>();
+
+    public void addQuery(String query) {
+
+        queryFreq.put(query,
+                queryFreq.getOrDefault(query, 0) + 1);
+    }
+
+    public List<String> search(String prefix) {
+
+        PriorityQueue<String> pq =
+                new PriorityQueue<>((a,b) ->
+                        queryFreq.get(b) - queryFreq.get(a));
+
+        for (String q : queryFreq.keySet()) {
+
+            if (q.startsWith(prefix))
+                pq.add(q);
+        }
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < 10 && !pq.isEmpty(); i++)
+            result.add(pq.poll());
+
+        return result;
+    }
 }
