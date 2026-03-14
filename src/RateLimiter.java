49e03a38668b6f32323b0584358d5e1fcfa61409
AG-0015 <agajalakshmi07@gmail.com>
2026-03-14 13:04:48 +0530

    Problem6: API rate limiter

--- src/RateLimiter.java
+++ src/RateLimiter.java
@@ -1,2 +1,33 @@
-public class RateLimiter {
+import java.util.*;
+
+class TokenBucket {
+
+    int tokens;
+    int maxTokens;
+    long lastRefill;
+
+    TokenBucket(int max) {
+        tokens = max;
+        maxTokens = max;
+        lastRefill = System.currentTimeMillis();
+    }
+}
+
+class RateLimiter {
+
+    HashMap<String, TokenBucket> clients = new HashMap<>();
+    int LIMIT = 1000;
+
+    public boolean checkRateLimit(String clientId) {
+
+        clients.putIfAbsent(clientId, new TokenBucket(LIMIT));
+        TokenBucket bucket = clients.get(clientId);
+
+        if (bucket.tokens > 0) {
+            bucket.tokens--;
+            return true;
+        }
+
+        return false;
+    }
 }
